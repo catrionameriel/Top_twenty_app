159fd31a7c72a4291e4695ff015382ee2ff33583
catrionameriel <catriona.meriel@gmail.com>
2018-02-20 19:15:34 +0000

    added image

--- src/components/TopTwentyEntry.js
+++ src/components/TopTwentyEntry.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const TopTwentyEntry = (props) => {
-  console.log(props);
-  return <p>{props.position}. {props.title} {props.artist}</p>
+  console.log(props.img[0].label);
+
+  return (
+    <div>
+      <img src={props.img[0].label} alt={props.artist}/>
+      <p>{props.position}. {props.title} {props.artist}</p>
+    </div>
+  )
 };
 
 export default TopTwentyEntry;
--- src/components/TopTwentyList.js
+++ src/components/TopTwentyList.js
@@ -5,7 +5,7 @@ const TopTwentyList = (props) => {
 
   let listItems = props.topTwentyList.map((song, index) => {
     return (
-      <TopTwentyEntry key={index} position={index+1} title={song['im:name'].label} artist={song['im:artist'].label} />
+      <TopTwentyEntry key={index} img={song['im:image']} position={index+1} title={song['im:name'].label} artist={song['im:artist'].label} />
     )
   })
 
